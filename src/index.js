82211e9882ee30f94795d44e07896c9f9c5654b1
ahmederrami <ahmederrami@gmail.com>
2023-12-05 07:39:48 +0100

    basic code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-function Car() {
-  return <h2>Hi, I am a Car!</h2>;
+function Car(props) {
+  return <h2>Hi, I am a {props.color} Car!</h2>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Car />);
+root.render(<Car color="red"/>);
